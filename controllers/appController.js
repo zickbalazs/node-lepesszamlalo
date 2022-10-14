8d0518a86c39379852504b84608c5e21762c29b0
zickbalazs <balazs.zick2@gmail.com>
2022-10-14 09:58:23 +0200

    Routing

--- controllers/appController.js
+++ controllers/appController.js
@@ -4,12 +4,17 @@ const ejs = require('ejs');
 const cfg = require('../config')
 
 router.get('/', (req,res)=>{
-    ejs.renderFile('./views/pages/index.ejs', ({title:cfg.config.title, author:cfg.author, company:cfg.company}), (err,data)=>{
+    ejs.renderFile('./views/pages/index.ejs', ({data:cfg.config}), (err,data)=>{
         if (err) res.status(500).send(err);
         else res.status(200).send(data);
     })
 })
-
+router.get('/reg', (req,res)=>{
+    ejs.renderFile('./views/pages/register.ejs', ({data:cfg.config}), (err,data)=>{
+        if (err) res.status(500).send(err);
+        else res.status(200).send(data);
+    })
+})
 
 
 module.exports = router;
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const mysql = require('mysql');
 const router = express.Router();
+const ejs = require('ejs');
+const cfg = require('../config');
+const sha1 = require('sha1')
 let connection = mysql.createPool(require('../config').pool);
 
 
@@ -10,7 +13,25 @@ router.get('/', (req,res)=>{
         else res.status(200).json(result);
     })
 })
+router.get('/login', (req,res)=>{
+    ejs.renderFile('./views/pages/dash.ejs', ({data:cfg.config}), (err,data)=>{
+        if (err) res.status(500).send(err);
+        else res.status(200).send(data);
+    });
+})
+router.get('/logout', (req,res)=>{
+    res.redirect('/')
+})
 
+router.post('/reg', (req,res)=>{
+    let userdata = {
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.passwd,
+        password2: req.body.passwd2
+    };
+    let list_of_errors = [];
+})
 
 
 module.exports = router;
